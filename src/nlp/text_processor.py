0d7d6bbefebb2516976cccea78677b6a1b372927
Michał Żarnecki <michalzarnecki88@gmail.com>
2019-03-22 21:35:20 +0100

    Update text_processor.py

--- src/nlp/text_processor.py
+++ src/nlp/text_processor.py
@@ -3,8 +3,6 @@ from src.nlp.util.punctuation_remover import PunctuationRemover
 from src.nlp.util.numeric_remover import NumericRemover
 from src.nlp.util.lowercase_applier import LowercaseApplier
 from src.nlp.util.stop_word_remover import StopWordRemover
-# from src.nlp.util.rare_word_remover import RareWordRemover
-# from src.nlp.util.frequent_word_remover import FrequentWordRemover
 
 class TextProcessor:
 
@@ -21,8 +19,6 @@ class TextProcessor:
         self.numericRemover = NumericRemover()
         self.lowercaseApplier = LowercaseApplier()
         self.stopWordRemover = StopWordRemover()
-        # self.rareWordRemover = RareWordRemover()
-        # self.frequentWordRemover = FrequentWordRemover()
         self.lemmatizer = Lemmatizer()
 
     def normalize(self, data):
@@ -30,7 +26,5 @@ class TextProcessor:
         data = self.punctuationRemover.process(data)
         data = self.lowercaseApplier.process(data)
         data = self.stopWordRemover.process(data)
-        # data = self.frequentWordRemover.process(data)
-        # data = self.rareWordRemover.process(data)
         data = self.lemmatizer.process(data)
         return data
